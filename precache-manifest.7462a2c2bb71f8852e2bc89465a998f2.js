d57504d11deb01506f0e61565d7390a8add76e0a
iamshiv007 <116198388+iamshiv007@users.noreply.github.com>
2022-11-24 16:24:08 +0530

    Updates

---
+++ precache-manifest.7462a2c2bb71f8852e2bc89465a998f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b699329530161ecd13db",
+    "url": "/Newsmonkey/static/css/main.3154a6ae.chunk.css"
+  },
+  {
+    "revision": "b699329530161ecd13db",
+    "url": "/Newsmonkey/static/js/main.b6993295.chunk.js"
+  },
+  {
+    "revision": "4ccff42df7664b317066",
+    "url": "/Newsmonkey/static/js/1.4ccff42d.chunk.js"
+  },
+  {
+    "revision": "8032f34af68a59664a1a",
+    "url": "/Newsmonkey/static/js/2.8032f34a.chunk.js"
+  },
+  {
+    "revision": "999310f0c88ffe860f5e",
+    "url": "/Newsmonkey/static/js/runtime~main.999310f0.js"
+  },
+  {
+    "revision": "763d516cf461880575d2292b11d03a92",
+    "url": "/Newsmonkey/static/media/not-found.763d516c.png"
+  },
+  {
+    "revision": "1a19f2718d3d4d5e04b8cb08a4d4cf18",
+    "url": "/Newsmonkey/static/media/image.1a19f271.gif"
+  },
+  {
+    "revision": "4d28cd5c0b4c6a767e7a2e9202a13d0f",
+    "url": "/Newsmonkey/index.html"
+  }
+];
